989f452fd110f674af07eb8b14f60e309e8fff07
Michał Pogoda <michalpogoda@hotmail.com>
2024-09-29 06:31:56 +0200

    configurable port

--- speech_grade/src/speech_grade/app.py
+++ speech_grade/src/speech_grade/app.py
@@ -52,4 +52,4 @@ async def analyze_video(video: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("SPEECH_GRADE_PORT", "8000")))
